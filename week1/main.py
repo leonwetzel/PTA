2094243a45ae0df969b799dfab453480a38b9952
Leon Wetzel <leonwetzel@gmail.com>
2020-04-14 10:10:32 +0200

    + Structuur van 1B toegevoegd

--- week1/main.py
+++ week1/main.py
@@ -3,22 +3,51 @@ import nltk
 
 
 def main():
+    """
+    Script for assignment 1 of week 1 for the course Project Text Analysis.
+    This script processes holmes.txt and retrieves information such as
+
+    :return:
+    """
+    with open("holmes.txt") as file:
+        data = file.read()
+
+    print(data)
+
+
+def prepare(data):
     pass
 
 
-def longest_sentence():
+def longest_sentence(sentences):
     pass
 
 
-def shortest_sentence():
+def shortest_sentence(sentences):
     pass
 
 
-def sentence_length_distribution():
+def sentence_length_distribution(sentences):
     pass
 
 
-def average_sentence_length():
+def average_sentence_length(sentences):
+    pass
+
+
+def character_types(ngrams):
+    pass
+
+
+def word_types(ngrams):
+    pass
+
+
+def top_twenty_character_level_unigrams_bigrams_trigrams(ngrams):
+    pass
+
+
+def top_twenty_word_level_unigrams_bigrams_trigrams(ngrams):
     pass
 
 
