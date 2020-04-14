56bc321d47f48304bef30c2177cc1a2f0274a806
Leon Wetzel <leonwetzel@gmail.com>
2020-04-14 09:59:39 +0200

    + Structuur van 1A toegevoegd

---
+++ week1/main.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+import nltk
+
+
+def main():
+    pass
+
+
+def longest_sentence():
+    pass
+
+
+def shortest_sentence():
+    pass
+
+
+def sentence_length_distribution():
+    pass
+
+
+def average_sentence_length():
+    pass
+
+
+if __name__ == '__main__':
+    main()
