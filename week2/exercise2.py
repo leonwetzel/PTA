ec33b8b13963d94316a83d1ef9d29e9cd41d0e09
Romanterp <romanterpstra@gmail.com>
2020-05-04 23:04:48 +0200

    exercise 2.3 and 2.4, added to md

--- week2/exercise2.py
+++ week2/exercise2.py
@@ -143,6 +143,7 @@ def main():
         holmes = file.read()
         holmes = nltk.word_tokenize(holmes)
         pos_tagged = nltk.pos_tag(holmes)
+        #print(pos_tagged)
 
     print("==============================================")
     print("ASSIGNMENT 2 - PART 4")
@@ -160,12 +161,11 @@ def main():
     pos_tag_list = []
     for word in br_tw:
         pos_tag_list.append(word[1])
-    text = nltk.Text(pos_tag_list)
 
-    bigram_ass_measures = nltk.collocations.BigramAssocMeasures()
+    bigram_assoc_measures = nltk.collocations.BigramAssocMeasures()
     pos_collocations = BigramCollocationFinder.from_words(pos_tag_list)
 
-    top_five = pos_collocations.nbest(bigram_ass_measures.pmi, 5)
+    top_five = pos_collocations.nbest(bigram_assoc_measures.pmi, 5)
     print(top_five)
 
     top_five_raw = pos.collocations.ngram_fd.most_common(5)
