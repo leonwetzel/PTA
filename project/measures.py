c632532345f31b3dd926bb95ad34e847328afe3c
Romanterp <romanterpstra@gmail.com>
2020-06-12 16:52:51 +0200

    measures

---
+++ project/measures.py
@@ -0,0 +1,84 @@
+#!/usr/bin/python3
+__author__ = "Leon Wetzel, Teun Buijse and Roman Terpstra"
+__copyright__ = "Copyright 2020 - Leon Wetzel, Teun Buijse and Roman Terpstra"
+__credits__ = ["Leon Wetzel", "Teun Buijse", "Roman Terpstra"]
+__license__ = "GPL"
+__version__ = "0.1"
+__email__ = ["l.f.a.wetzel@student.rug.nl", "t.c.buijse@student.rug.nl",
+             "r.p.terpstra@student.rug.nl"]
+__status__ = "Development"
+
+import os
+from collections import Counter
+from nltk.metrics import ConfusionMatrix
+
+
+def scores(labels, cm):
+
+    true_positives = Counter()
+    false_negatives = Counter()
+    false_positives = Counter()
+
+    for i in labels:
+        for j in labels:
+            if i == j:
+                true_positives[i] += cm[i, j]
+            else:
+                false_negatives[i] += cm[i, j]
+                false_positives[j] += cm[i, j]
+
+    print("TP:", sum(true_positives.values()), true_positives)
+    print("FN:", sum(false_negatives.values()), false_negatives)
+    print("FP:", sum(false_positives.values()), false_positives)
+    print()
+
+    for i in sorted(labels):
+        if true_positives[i] == 0:
+            fscore = 0
+        else:
+            precision = true_positives[i] / float(true_positives[i] + false_positives[i])
+            recall = true_positives[i] / float(true_positives[i] + false_negatives[i])
+            fscore = 2 * (precision * recall) / float(precision + recall)
+        print(i)
+        print('f-score:', fscore)
+        print('recall:', recall)
+        print('precision:', precision)
+        print()
+
+
+def annotations(directory):
+    classeslst = []
+    wikilst = []
+    for root, dirs, files in os.walk(directory):
+        for name in files:
+            if name.endswith(".ent"):
+                input_file_path = os.path.join(root, name)
+
+                with open(input_file_path, 'r') as file:
+                    lines = file.readlines()
+                    for line in lines:
+                        if len(line.split()) >= 6:
+                            classeslst.append(line.split()[5])
+                            #if len(line.split()) == 7:
+                                #wikilst.append(line.split()[6])
+                            #else:
+                                #continue
+                        else:
+                            continue
+
+    return classeslst
+
+
+def main():
+    items1 = annotations("dev")
+    items2 = annotations("test")
+
+    cm = ConfusionMatrix(items1, items2)
+    print(cm)
+
+    labels_class = set('COU CIT NAT PER ORG ENT'.split())
+    scores(labels_class, cm)
+
+
+if __name__ == '__main__':
+    main()
